250b7e9429964e3cc96cbba998696043f22caf20
pisc9211 <pisc9211@gmail.com>
2019-09-11 18:03:19 -0400

    added favicon

--- js/main.js
+++ js/main.js
@@ -90,84 +90,84 @@
 
 
 
-	var sliderMain = function() {
+	// var sliderMain = function() {
 		
-	  	$('#colorlib-hero .flexslider').flexslider({
-			animation: "fade",
-			slideshowSpeed: 5000,
-			directionNav: true,
-			start: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			},
-			before: function(){
-				setTimeout(function(){
-					$('.slider-text').removeClass('animated fadeInUp');
-					$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
-				}, 500);
-			}
+	//   	$('#colorlib-hero .flexslider').flexslider({
+	// 		animation: "fade",
+	// 		slideshowSpeed: 5000,
+	// 		directionNav: true,
+	// 		start: function(){
+	// 			setTimeout(function(){
+	// 				$('.slider-text').removeClass('animated fadeInUp');
+	// 				$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
+	// 			}, 500);
+	// 		},
+	// 		before: function(){
+	// 			setTimeout(function(){
+	// 				$('.slider-text').removeClass('animated fadeInUp');
+	// 				$('.flex-active-slide').find('.slider-text').addClass('animated fadeInUp');
+	// 			}, 500);
+	// 		}
 
-	  	});
+	//   	});
 
-	};
+	// };
 
-	var stickyFunction = function() {
+	// var stickyFunction = function() {
 
-		var h = $('.image-content').outerHeight();
+	// 	var h = $('.image-content').outerHeight();
 
-		if ($(window).width() <= 992 ) {
-			$("#sticky_item").trigger("sticky_kit:detach");
-		} else {
-			$('.sticky-parent').removeClass('stick-detach');
-			$("#sticky_item").trigger("sticky_kit:detach");
-			$("#sticky_item").trigger("sticky_kit:unstick");
-		}
+	// 	if ($(window).width() <= 992 ) {
+	// 		$("#sticky_item").trigger("sticky_kit:detach");
+	// 	} else {
+	// 		$('.sticky-parent').removeClass('stick-detach');
+	// 		$("#sticky_item").trigger("sticky_kit:detach");
+	// 		$("#sticky_item").trigger("sticky_kit:unstick");
+	// 	}
 
-		$(window).resize(function(){
-			var h = $('.image-content').outerHeight();
-			$('.sticky-parent').css('height', h);
+	// 	$(window).resize(function(){
+	// 		var h = $('.image-content').outerHeight();
+	// 		$('.sticky-parent').css('height', h);
 
 
-			if ($(window).width() <= 992 ) {
-				$("#sticky_item").trigger("sticky_kit:detach");
-			} else {
-				$('.sticky-parent').removeClass('stick-detach');
-				$("#sticky_item").trigger("sticky_kit:detach");
-				$("#sticky_item").trigger("sticky_kit:unstick");
+	// 		if ($(window).width() <= 992 ) {
+	// 			$("#sticky_item").trigger("sticky_kit:detach");
+	// 		} else {
+	// 			$('.sticky-parent').removeClass('stick-detach');
+	// 			$("#sticky_item").trigger("sticky_kit:detach");
+	// 			$("#sticky_item").trigger("sticky_kit:unstick");
 
-				$("#sticky_item").stick_in_parent();
-			}
+	// 			$("#sticky_item").stick_in_parent();
+	// 		}
 			
 
 			
 
-		});
+	// 	});
 
-		$('.sticky-parent').css('height', h);
+	// 	$('.sticky-parent').css('height', h);
 
-		$("#sticky_item").stick_in_parent();
+	// 	$("#sticky_item").stick_in_parent();
 
-	};
+	// };
 
-	var owlCrouselFeatureSlide = function() {
-		$('.owl-carousel').owlCarousel({
-			animateOut: 'fadeOut',
-		   animateIn: 'fadeIn',
-		   autoplay: true,
-		   loop:true,
-		   margin:0,
-		   nav:true,
-		   dots: false,
-		   autoHeight: true,
-		   items: 1,
-		   navText: [
-		      "<i class='icon-arrow-left3 owl-direction'></i>",
-		      "<i class='icon-arrow-right3 owl-direction'></i>"
-	     	]
-		})
-	};
+	// var owlCrouselFeatureSlide = function() {
+	// 	$('.owl-carousel').owlCarousel({
+	// 		animateOut: 'fadeOut',
+	// 	   animateIn: 'fadeIn',
+	// 	   autoplay: true,
+	// 	   loop:true,
+	// 	   margin:0,
+	// 	   nav:true,
+	// 	   dots: false,
+	// 	   autoHeight: true,
+	// 	   items: 1,
+	// 	   navText: [
+	// 	      "<i class='icon-arrow-left3 owl-direction'></i>",
+	// 	      "<i class='icon-arrow-right3 owl-direction'></i>"
+	//      	]
+	// 	})
+	// };
 
 	// Document on load.
 	$(function(){
@@ -178,10 +178,10 @@
 		// windowScroll();
 
 
-		mobileMenuOutsideClick();
-		sliderMain();
-		stickyFunction();
-		owlCrouselFeatureSlide();
+		// mobileMenuOutsideClick();
+		// sliderMain();
+		// stickyFunction();
+		// owlCrouselFeatureSlide();
 	});
 
 
